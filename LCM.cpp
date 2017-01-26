1fe221392e63342e5a75b403e0c8d0d9ef378524
Aditya Kumar <kaditya139@gmail.com>
2017-01-26 16:04:25 +0530

    Update LCM.cpp

--- LCM.cpp
+++ LCM.cpp
@@ -1,3 +1,6 @@
+//This code is for finding the LCM of two or more number. You have to provide the number of input and inputs to get the LCM.
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -5,7 +8,7 @@ int main() {
     int n,i;
     cout<<"Enter size of array"<<'\t';
     cin>>n;
-    int store[n],factor[n];
+    int store[n],factor[n];     //'store' stores the number povided by user. 
     for(i=0;i<n;i++)
         cin>>store[i];
     for(i=0;i<n;i++)
@@ -13,14 +16,14 @@ int main() {
     int j,flag=0,l=0,max_num=0;
     for(i=0;i<n;i++){
         if(max_num<store[i])
-            max_num=store[i];
+            max_num=store[i];       //'max_num finds the maximum value in the array
     }
     i=2;
     while(i<=max_num/2){
         flag=0;
         for(j=0;j<n;j++){
             if(store[j]%i==0){
-                flag++;
+                flag++;          //'flag' variable is used for checking whether two or more number is divisibe by a particular number or not.
             }
         }
         if(flag>=2){
@@ -29,7 +32,7 @@ int main() {
                     store[j]=store[j]/i;
                 }
             }
-            factor[l++]=i;
+            factor[l++]=i;          //If any number divides two or more number in array, it gets stored in 'factor' array.
         }
         else
             i++;
@@ -39,7 +42,7 @@ int main() {
         product=product*factor[i];
     for(i=0;i<n;i++)
         product=product*store[i];
-    cout<<product<<'\n';
+    cout<<product<<'\n';            //Lastly finds product of both the array and dispay it.
 
     return 0;
 }
